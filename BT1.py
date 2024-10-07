2031071f4fdadc12ddfaf794bf19dbe9adf73b60
TrinhKhoiNguyen <23162068@student.hcmute.edu.vn>
2024-10-07 23:47:17 +0700

    Update BT1.py

--- BT1.py
+++ BT1.py
@@ -1,7 +1,7 @@
 a,b,c = map(int,input("Nhap vao 1 pt: ").split())
 
 ketQua = a+b+c
-print("Ket qua cua bai toan tren la: ",ketQua)print("Ket qua cua bai toan tren la: ",ketQua)
+print("Ket qua cua bai toan tren la: ",ketQua)
 
 
 print("Tui gion do!!!")
