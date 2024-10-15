0dbe33e59d21e9b70ce5c6ad4d1e7e93af2f0fe2
TrinhKhoiNguyen <23162068@student.hcmute.edu.vn>
2024-10-15 18:01:53 +0700

    test new

---
+++ wwww.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+    public string nh;
+    public int soLuong;
+    public int soRuong;
+
+    public Class1(string nh, int soLuong)
+    {
+        this.nh = nh;
+        this.soLuong = soLuong;
+        this.soRuong = soRuong;
+    }
+    
+}
